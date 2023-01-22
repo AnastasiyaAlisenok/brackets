23f86a05dc839f30ea039421f6048ae5e9d3def3
AnastasiyaAlisenok <strelkinz1@yandex.ru>
2023-01-22 17:59:44 +0300

    feat: add js code

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-module.exports = function check(str, bracketsConfig) {
+function check(str, bracketsConfig) {
   bracketsConfig = Object.fromEntries(bracketsConfig);
   str = [...str];
   
@@ -12,9 +12,17 @@ module.exports = function check(str, bracketsConfig) {
   for(let i=0; i<str.length; i++) {
     let bracket = str[i];
 
-    if(bracket === '|') {
+    if(str[i] === '|' && str[i+1] === '|') {
+      str.splice(i, i+1);
+    } else if(str[i] === '|') {
       s.push(bracketsConfig[bracket])
-    } else if(bracket === '7' || bracket === '8') {
+    } else if(bracket === '7' && str[i+1] === '7') {
+      str.splice(i, i+1);
+    } else if(bracket === '7') {
+      n.push(bracketsConfig[bracket]);
+    } else if(bracket === '8' && str[i+1] === '8') {
+      str.splice(i, i+1);
+    } else if(bracket === '8') {
       n.push(bracketsConfig[bracket]);
     } else if(bracketsConfig[bracket]) {
       stack.push(bracketsConfig[bracket])
@@ -25,6 +33,13 @@ module.exports = function check(str, bracketsConfig) {
     }
 }}
 console.log(stack)
+console.log(s)
+console.log(n)
 return stack.length === 0 && s.length%2 === 0 && n.length%2 === 0;
 }
 
+
+
+  str = '|(|)';
+  bracketsConfig =[['(', ')'], ['|', '|']];
+  console.log(check(str, bracketsConfig));
